5fd96360b87c9f33211f42a99ac0ea02aecdb7ab
Moataz-Mahmoud <moatazmasters@gmail.com>
2019-08-05 02:59:52 -0500

    add directConnect: true to config file

--- trivago_e2e/config.js
+++ trivago_e2e/config.js
@@ -1,7 +1,7 @@
 exports.config = {
     //seleniumAddress: 'http://localhost:4444/wd/hub', 
     specs: ['./specs/purchaseTest.js'],
-    useAllAngular2AppRoots: true,
+    directConnect: true,
     params: {
         homepage: {
             searchItem: 'LUNDHAGS'
